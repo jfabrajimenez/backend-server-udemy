3a55c8edc96820f5e5382e5d1203aac1714c5b27
jose <jfabrajimenez@gmail.com>
2019-07-10 08:38:19 +0200

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // Requires
 var express = require('express');
-
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
-var mongoose = require('mongoose');
 
+//Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 //Conexión a la BBDD
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -13,15 +22,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 });
 
-
 //Rutas
-app.get('/', (request, response, next) => {
-    response.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-});
-
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 // Escuchar peticiones
 app.listen(3000, () => {
