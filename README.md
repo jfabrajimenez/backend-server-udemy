cbfa8c1b0b6dfcd949ef17d06799f34e9f9f30c4
jose <jfabrajimenez@gmail.com>
2019-07-11 19:17:10 +0200

    Fin de la sección 12

---
+++ README.md
@@ -0,0 +1,3 @@
+# Backend-Server
+
+Este es co código necesario para establecer el backen server conectado a mongo para el curso de Angular Avanzado
--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+//Google
+module.exports.CLIENT_ID = '154756128084-uhlvb0ua769vaq2ni671g4u597h3rs8g.apps.googleusercontent.com';
